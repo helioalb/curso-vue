cba1d5a4d54978032e4e98d3a8485e14ead79070
Helio Albano de Oliveira <helioalb@gmail.com>
2024-03-16 10:50:10 -0300

    Add event bus example

---
+++ componentes-comunicacao/comunicacao-bus/src/components/UsuarioEditar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="componente">
+        <h2>Alterar os Dados de Usuário</h2>
+        <p>Edite as informações</p>
+        <p>Idade do usuario <strong>{{ idade }}</strong></p>
+        <button @click="alterarIdade">Alterar idade</button>
+    </div>
+</template>
+
+<script>
+import barramento from '../barramento';
+export default {
+    props:  {
+        idade: Number
+    },
+    methods: {
+        alterarIdade() {
+            this.idade = 33
+            barramento.$emit('idadeMudou', this.idade)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #98b99a;
+        color: #fff;
+    }
+</style>
---
+++ componentes-comunicacao/comunicacao-bus/src/components/UsuarioInfo.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do usuário: <strong>{{ invertNome() }}</strong></p>
+        <p>Idade do usuario <strong>{{ idade }}</strong></p>
+        <button @click="reinicializarNome">Reiniciar Nome</button>
+        <button @click="reiniciarFn">Reiniciar Nome (Callback)</button>
+    </div>
+</template>
+
+<script>
+import barramento from '../barramento'
+
+export default {
+    props: {
+        nome: {
+            type: String,
+            // required: true
+            default: 'Anonimo'
+        },
+        reiniciarFn: Function,
+        idade: Number
+    },
+    methods: {
+        invertNome() {
+            return this.nome.split('').reverse().join('')
+        },
+        reinicializarNome() {
+            this.nome = 'Pedro'
+            this.$emit('mudouNome', this.nome)
+        }
+    },
+    created() {
+        barramento.$on('idadeMudou', idade => {
+            this.idade = idade
+        })        
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
